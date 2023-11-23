3c294ac9f53f3e8b603ed3780d4c50c306978f88
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-11-22 20:31:53 -0400

    initial nest todo

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
