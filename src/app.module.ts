a2678ed81e1e0797bb69b215da481ba809a71728
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-11-22 21:31:37 -0400

    feat: apollo server and todo module-resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,19 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+    TodoModule,
   ],
-  controllers: [],
   providers: [],
 })
 export class AppModule {}
