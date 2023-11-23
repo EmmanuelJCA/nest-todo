cdb635e4fbb7344128a2841d81064b146c275cf9
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-11-22 22:02:54 -0400

    feat: query to find all todos

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+
 import { TodoResolver } from './todo.resolver';
+import { TodoService } from './todo.service';
 
 @Module({
-  providers: [TodoResolver],
+  providers: [TodoResolver, TodoService],
 })
 export class TodoModule {}
--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,4 +1,26 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Todo } from './entity/todo.entity';
+
+import { TodoService } from './todo.service';
 
 @Resolver()
-export class TodoResolver {}
+export class TodoResolver {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Query(() => [Todo], { name: 'todos' })
+  findAll(): Todo[] {
+    return this.todoService.findAll();
+  }
+
+  findOne() {
+    return {};
+  }
+
+  create() {
+    return {};
+  }
+
+  update() {}
+
+  remove() {}
+}
